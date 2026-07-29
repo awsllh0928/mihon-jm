28be8afbe251d60bc6296b2e90aa40fa37056ec1
awsllh0928 <154075620+awsllh0928@users.noreply.github.com>
2026-07-29 10:06:46 +0800

    Create build.gradle.kts

---
+++ src/zh/jm/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("tachiyomi.source")
+}
+
+tachiyomi {
+    name = "禁漫天堂"
+    lang = "zh"
+    version = "1.0.0"
+    minAppVersion = "1.5.0"
+    baseUrl = "https://18comic.vip"
+    description = "禁漫天堂 - 18comic"
+}
